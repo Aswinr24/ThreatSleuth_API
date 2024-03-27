3b760d5dbc3b7a12f90a3d870d76d2ed404d953d
R Aswin <aswinr242004@gmail.com>
2024-03-27 23:50:12 +0530

    Add application file

--- api.py
+++ api.py
@@ -12,12 +12,12 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import string
 import sklearn
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 stemmer = PorterStemmer()
 
 app = FastAPI()
 
+stopwords=['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 class URLInput(BaseModel):
     url: str
@@ -49,7 +49,7 @@ def transform(text):
     text = current[:]
     current.clear()
     for i in text:
-        if i not in ENGLISH_STOP_WORDS and i not in string.punctuation:
+        if i not in stopwords and i not in string.punctuation:
             current.append(i)
     for i in text:
         current.append(stemmer.stem(i))
