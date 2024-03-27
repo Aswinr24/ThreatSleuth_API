c9cbf5df2e3a6a1b0a0f628e337ac797b0f56de3
R Aswin <aswinr242004@gmail.com>
2024-03-27 14:10:55 +0530

    added files

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from dotenv import load_dotenv
+import os
 import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+load_dotenv()
+
+PORT = int(os.get('PORT', 8000))
+HOST = '0.0.0.0'
+
+if __name__ == '__main__':
+    uvicorn.run('app.api:app', host = HOST, port = PORT, reload = True)
